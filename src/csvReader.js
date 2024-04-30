1525f6ecff841b252c1af273675f893a790aea50
mrunalinee1 <mrunalineekhairnar@gmail.com>
2024-04-30 23:54:00 +0530

    3

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-function readCSV(filePath) {
+async function readCSV(filePath) {
     const results = [];
 
     return new Promise((resolve, reject) => {
